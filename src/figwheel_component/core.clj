ba85aaf36d5adc8969a664b7c155b5d2e99d3cf2
Kimmo Koskinen <kimmo@tiuhti.net>
2015-09-05 22:39:10 +0300

    Fix name collision

--- src/figwheel_component/core.clj
+++ src/figwheel_component/core.clj
@@ -3,18 +3,18 @@
             [figwheel-sidecar.core :as core]
             [figwheel-sidecar.repl-api :as repl]))
 
-(defrecord Figwheel [build]
+(defrecord Figwheel [build-id]
   component/Lifecycle
   (start [this]
     (if (:started this)
       this
       (let [project (->> "project.clj" slurp read-string (drop 3) (apply hash-map))
             figwheel-options (-> project :figwheel)
-            build (-> project :cljsbuild :builds build)
+            build (-> project :cljsbuild :builds build-id)
             {:keys [source-paths compiler]} build]
         (repl/start-figwheel! {:figwheel-options figwheel-options
-                               :build-ids [(name build)]
-                               :all-builds [{:id (name build)
+                               :build-ids [(name build-id)]
+                               :all-builds [{:id (name build-id)
                                              :source-paths source-paths
                                              :compiler compiler}]})
         (repl/start-autobuild)
@@ -22,5 +22,5 @@
   (stop [this]
     (assoc this :started nil)))
 
-(defn figwheel-component [build]
-  (->Figwheel build))
+(defn figwheel-component [build-id]
+  (->Figwheel build-id))
